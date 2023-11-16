f79dd5730e7f0182db780e71404fd529ed7301a4
SOHAM SARKAR <pes2202100825@pesu.pes.edu>
2023-11-16 13:27:07 +0530

    Add files via upload

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -63,11 +63,11 @@ async function handleLogin(req, res) {
                 if (value.length === 8 || value.length === 13) {
                     return value;
                 } else {
-                    return helper.message("invalid ID")
+                    return helper.message("invalid ID");
                 }
             }).required(),
             pwd: Joi.string().min(5).required()
-        })
+        });
 
         let check = schema.validate(req.body);
         if (check.hasOwnProperty("error")) {
@@ -82,38 +82,35 @@ async function handleLogin(req, res) {
         }
 
         let typeOfUsr = "student";
+        let typeOfId = "srn";
         if (req.body.id.length === 8) {
             typeOfUsr = "faculty";
+            typeOfId = "id";
         }
 
         let result, _, connection = await getConnection();
-        if (typeOfUsr === "student") {
-            [result, _] = await connection.execute(
-                "SELECT `srn`, `password` FROM `student` WHERE `srn` = ?",
-                [req.body.id]
-            );
-        } else {
-            [result, _] = await connection.execute(
-                "SELECT `id`, `password` FROM `faculty` WHERE `id` = ?",
-                [req.body.id]
-            );
-        }
+        const sqlQuery =
+            `SELECT ${typeOfId}, password, first_name, last_name
+             FROM ${typeOfUsr}
+             WHERE ${typeOfId} = '${req.body.id}'`;
+        [result, _] = await connection.query(sqlQuery);
 
-        let found = false;
+
         let valid = false;
+        let name = "";
         if (result.length !== 0) {
-            found = true;
             let actual_pwd = result[0].password;
             if (actual_pwd === req.body.pwd) {
                 valid = true;
+                name = result[0].first_name + " " + result[0].last_name;
             }
         }
 
         let response = {
             status: true,
-            found: found,
             valid: valid,
-            type: typeOfUsr
+            type: typeOfUsr,
+            name: name
         };
 
         console.log("Success, ", response);
@@ -129,33 +126,30 @@ async function handleRegister(req, res) {
     try {
         console.log("Got request", req.body);
 
-        let invalid_request = false;
-        if (!req.body.hasOwnProperty("id") ||
-            !req.body.hasOwnProperty("pwd") ||
-            !req.body.hasOwnProperty("type")) {
-            // !(req.body.type.toLowerCase() in ['student', 'faculty'])
-            // !req.body.hasOwnProperty("details") ||
-            // !req.body.details.hasOwnProperty("firstName") ||
-            // !req.body.details.hasOwnProperty("lastName") ||
-            // !req.body.details.hasOwnProperty("dept") ||
-            // !(req.body.details.dept.toLowerCase() in ['cse', 'ece', 'eee', 'aiml', 'me']) ||
-            // (req.body.type === 'student' && (
-            //     !req.body.details.hasOwnProperty("sem") ||
-            //     !req.body.details.hasOwnProperty("sec")
-            // )) ||
-            // (req.body.type === 'faculty' && (
-            //     !req.body.details.hasOwnProperty("domain")
-            // ))) {
-            invalid_request = true;
-        }
+        const schema = Joi.object({
+            id: Joi.string().alphanum().custom((value, helper) => {
+                if (value.length === 8 || value.length === 13) {
+                    return value;
+                } else {
+                    return helper.message("invalid ID");
+                }
+            }).required(),
+            pwd: Joi.string().min(5).required()
+        });
 
-        if (invalid_request) {
-            let response = {invalidRequest: true, status: false, errMsg: "refer API properly"};
+        let check = schema.validate(req.body);
+        if (check.hasOwnProperty("error")) {
+            let response = {
+                invalidRequest: true,
+                status: false,
+                errMsg: check.error.details[0].message
+            };
             console.log("InvalidRequest, ", response);
             res.send(response);
             return;
         }
 
+        console.log('passed');
         let result, _, connection = await getConnection();
 
         if (req.body.type === "student") {
@@ -172,20 +166,19 @@ async function handleRegister(req, res) {
             //     ]
             // );
             // console.log(result, _);
-            console.log('passed')
         } else {
-            [result, _] = await connection.execute(
-                "INSERT INTO `faculty` VALUES (?, ?, ?, ?, ?, ?, ?)",
-                [
-                    req.body.id,
-                    req.body.details.firstName,
-                    req.body.details.lastName,
-                    req.body.details.dept.toUpperCase(),
-                    req.body.details.domain,
-                    req.body.pwd
-                ]
-            );
-            console.log(result, _);
+            // [result, _] = await connection.execute(
+            //     "INSERT INTO `faculty` VALUES (?, ?, ?, ?, ?, ?, ?)",
+            //     [
+            //         req.body.id,
+            //         req.body.details.firstName,
+            //         req.body.details.lastName,
+            //         req.body.details.dept.toUpperCase(),
+            //         req.body.details.domain,
+            //         req.body.pwd
+            //     ]
+            // );
+            // console.log(result, _);
         }
 
         let response = {status: true};
@@ -203,10 +196,15 @@ async function handleTest(req, res) {
     try {
         console.log("Got request", req.body);
 
-        let invalid_request = false;
-        // TODO Validate Request
-        if (invalid_request) {
-            let response = {invalidRequest: true, status: false, errMsg: "refer API properly"};
+        const schema = Joi.object({});
+
+        let check = schema.validate(req.body);
+        if (check.hasOwnProperty("error")) {
+            let response = {
+                invalidRequest: true,
+                status: false,
+                errMsg: check.error.details[0].message
+            };
             console.log("InvalidRequest, ", response);
             res.send(response);
             return;
